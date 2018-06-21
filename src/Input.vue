2afb6f0916d9c53ada96fb9a28e96397aa6f19c3
Unknown <mb.prodeveloper@gmail.com>
2018-06-21 09:17:40 +0530

    first commit

---
+++ src/Input.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+	<input type="text" :value="message" @input="changeMessage">
+	<p>{{ msg }}</p>
+</div>
+</template>
+
+<script>
+	export default {
+		props: ['msg'],
+		data() {
+			return {
+				message: ''
+			}
+		},
+		methods: {
+			changeMessage(event) {
+				this.message = event.target.value;
+				this.$emit('messageChanged', this.message); // $ sign always tells that this is built-in method
+			}
+		}
+	}
+</script>
